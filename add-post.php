2c16fb0a60bbd01d280313ac7c475811ef04fc8e
shehryartalat <shehryartalat41@gmail.com>
2023-07-01 23:38:48 +0500

    allignment

--- add-post.php
+++ add-post.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>JobEntry - Job Portal</title>
     <link href="css/style.css" rel="stylesheet">
     <script>
         function validateForm() {
--- job-detail.php
+++ job-detail.php
@@ -3,7 +3,7 @@
 
 <head>
 <meta charset="utf-8">
-<title>JobEntry - Job Portal Website Template</title>
+<title>JobEntry - Job Portal</title>
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 <meta content="" name="keywords">
 <meta content="" name="description">
@@ -43,37 +43,7 @@
 
 
 <!-- Navbar Start -->
-<nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-<a href="index.php" class="navbar-brand d-flex align-items-center text-center py-0 px-4 px-lg-5">
-<h1 class="m-0 text-primary">JobEntry</h1>
-</a>
-<button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarCollapse">
-<div class="navbar-nav ms-auto p-4 p-lg-0">
-<a href="index.php" class="nav-item nav-link">Home</a>
-<a href="about.php" class="nav-item nav-link">About</a>
-<div class="nav-item dropdown">
-<a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Jobs</a>
-<div class="dropdown-menu rounded-0 m-0">
-<a href="job-list.php" class="dropdown-item">Job List</a>
-<a href="job-detail.php" class="dropdown-item active">Job Detail</a>
-</div>
-</div>
-<div class="nav-item dropdown">
-<a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-<div class="dropdown-menu rounded-0 m-0">
-<a href="category.php" class="dropdown-item">Job Category</a>
-<a href="testimonial.php" class="dropdown-item">Testimonial</a>
-<a href="404.php" class="dropdown-item">404</a>
-</div>
-</div>
-<a href="contact.php" class="nav-item nav-link">Contact</a>
-</div>
-<a href="" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Post A Job<i class="fa fa-arrow-right ms-3"></i></a>
-</div>
-</nav>
+<?php include 'Component/Navbar.php';?>
 <!-- Navbar End -->
 
 
@@ -180,68 +150,8 @@
 
 
 <!-- Footer Start -->
-<div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
-<div class="container py-5">
-<div class="row g-5">
-<div class="col-lg-3 col-md-6">
-<h5 class="text-white mb-4">Company</h5>
-<a class="btn btn-link text-white-50" href="">About Us</a>
-<a class="btn btn-link text-white-50" href="">Contact Us</a>
-<a class="btn btn-link text-white-50" href="">Our Services</a>
-<a class="btn btn-link text-white-50" href="">Privacy Policy</a>
-<a class="btn btn-link text-white-50" href="">Terms & Condition</a>
-</div>
-<div class="col-lg-3 col-md-6">
-<h5 class="text-white mb-4">Quick Links</h5>
-<a class="btn btn-link text-white-50" href="">About Us</a>
-<a class="btn btn-link text-white-50" href="">Contact Us</a>
-<a class="btn btn-link text-white-50" href="">Our Services</a>
-<a class="btn btn-link text-white-50" href="">Privacy Policy</a>
-<a class="btn btn-link text-white-50" href="">Terms & Condition</a>
-</div>
-<div class="col-lg-3 col-md-6">
-<h5 class="text-white mb-4">Contact</h5>
-<p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-<p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-<p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
-<div class="d-flex pt-2">
-<a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-<a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-<a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-<a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-</div>
-</div>
-<div class="col-lg-3 col-md-6">
-<h5 class="text-white mb-4">Newsletter</h5>
-<p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-<div class="position-relative mx-auto" style="max-width: 400px;">
-<input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-<button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-</div>
-</div>
-</div>
-</div>
-<div class="container">
-<div class="copyright">
-<div class="row">
-<div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-&copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
+<?php include 'Component/Footer.php';?>
 
-<!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you had like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
-</div>
-<div class="col-md-6 text-center text-md-end">
-<div class="footer-menu">
-<a href="">Home</a>
-<a href="">Cookies</a>
-<a href="">Help</a>
-<a href="">FQAs</a>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
 <!-- Footer End -->
 
 
